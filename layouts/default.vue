409e796d33caf6bb5e2ed74969734c2cc467506e
Mehran Sarabchian <sarabchian@axxessio.com>
2023-12-27 23:28:54 +0100

    adding modules and also i18n internationalitzation

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,16 +3,33 @@
     and this root element cannot be a <slot />. -->
 <template>
   <div>
+    <div>switch language</div>
+    <select v-model="language">
+      <option v-for="item in locales" :key="item" :value="item.code">
+        {{ item.name }}
+      </option>
+    </select>
     <nav>
       <ul>
         <li>
-          <NuxtLink to="/">Home</NuxtLink>
+          <NuxtLink :to="localPath('/')">Home</NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/about">About</NuxtLink>
+          <NuxtLink :to="localPath('/about')">About</NuxtLink>
         </li>
       </ul>
     </nav>
     <slot />
   </div>
 </template>
+
+<script setup>
+const { locales, locale, setLocale } = useI18n();
+const localPath = useLocalePath();
+const language = computed({
+  get: () => locale.value,
+  set: (value) => {
+    setLocale(value);
+  },
+});
+</script>
