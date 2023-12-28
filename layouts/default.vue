0a4410eb01ed687013b95c9c9f3cb0ff68ee278a
Mehran Sarabchian <sarabchian@axxessio.com>
2023-12-28 11:26:19 +0100

    Tailwind CSS

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,24 +2,34 @@
     to allow Nuxt to apply transitions between layout changes - 
     and this root element cannot be a <slot />. -->
 <template>
-  <div>
-    <div>switch language</div>
-    <select v-model="language">
-      <option v-for="item in locales" :key="item" :value="item.code">
-        {{ item.name }}
-      </option>
-    </select>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink :to="localPath('/')">Home</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink :to="localPath('/about')">About</NuxtLink>
-        </li>
-      </ul>
-    </nav>
-    <slot />
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
+          >Mehran Sarabchian</NuxtLink
+        >
+      </div>
+      <nav class="font-mono">
+        <ul class="flex space-x-4">
+          <li>
+            <NuxtLink :to="localPath('/')">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink :to="localPath('/about')">About</NuxtLink>
+          </li>
+          <li>
+            <select v-model="language">
+              <option v-for="item in locales" :key="item" :value="item.code">
+                {{ item.name }}
+              </option>
+            </select>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main class="p-2">
+      <slot />
+    </main>
   </div>
 </template>
 
